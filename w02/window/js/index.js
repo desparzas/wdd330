5b6476183a370feb5ce5e77b00c343d790af9dcf
desparzas <desparzas7902@gmail.com>
2025-03-15 08:18:09 -0500

    add fix

--- w02/window/js/index.js
+++ w02/window/js/index.js
@@ -10,9 +10,18 @@ hambutton.addEventListener(
 );
 
 // To solve the mid resizing issue with responsive class [window.onresize]
+
+let alertShown = false;
+
 window.addEventListener("resize", () => {
   if (window.innerWidth > 760) {
     // window.innerWidth includes the scrollbar (if any), document.documentElement.clientWidth does not
     mainnav.classList.remove("responsive");
+    if (!alertShown) {
+      window.alert("Responsive class removed due to window resizing.");
+      alertShown = true;
+    }
+  } else {
+    alertShown = false;
   }
 });
